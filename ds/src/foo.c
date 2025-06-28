d75e187c28ea77b906896839fbbc690d80fc0960
baruch.haimson <chaimsonb@gmail.com>
2025-06-29 00:12:47 +0300

    finish make file

---
+++ ds/src/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../include/foo.h"
+
+void printHello() {
+    printf("Hello Makefile!\n");
+}
+
---
+++ ds/test/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/foo.h"
+
+int main() {
+    printHello();
+    return 0;
+}
+
